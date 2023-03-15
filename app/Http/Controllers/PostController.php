49c7d87d63655e78ae40a2d10c5e4dfad8325b13
Azkaaja <bolbagantunahan22@gmail.com>
2023-03-15 13:24:01 +0700

    add post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,8 @@ namespace App\Http\Controllers;
 use App\Http\Resources\PostDetailResource;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
+use Illuminate\Auth\Events\Validated;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 class PostController extends Controller
 {
@@ -19,5 +21,16 @@ class PostController extends Controller
     public function show2($id){
         $post = Post::findOrFail($id);
         return new PostDetailResource($post);
+    }
+
+    public function store(Request $request){
+        $request ->validate([
+            'title' => 'required|max:225',
+            'news_content'=>'required',
+        ]);
+        //return response()->json('sudah dapat digunakan');
+        $request['author']= Auth::user()->id;
+        $post = Post::create($request->all());
+        return new PostDetailResource($post->loadMissing('writer:id,username'));
     }
 }
