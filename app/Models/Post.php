b15d0919327980deb24e7b5b04e75a6a6e478743
Azkaaja <bolbagantunahan22@gmail.com>
2023-03-17 09:24:02 +0700

    show comment on article & detailarticle

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
@@ -18,5 +19,8 @@ class Post extends Model
     public function writer(): BelongsTo
     {
         return $this->belongsTo(User::class, 'author', 'id');
+    }
+    public function comments(): HasMany{
+        return $this->hasMany(Comment::class,'post_id','id');
     }
 }
